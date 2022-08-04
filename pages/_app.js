6ee56cecfee55cc0aa67259c89b8b019809da750
lakshmankashyap <lkashyap92@gmail.com>
2022-08-04 11:10:03 +0530

    init

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
-import '../styles/globals.css'
+// pages/_app.js
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/globals.css";
+import { useState } from "react";
+import AppContext from "../AppContext";
+import languagesObject from "../languagesObject";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [languageSelected, setLanguageSelected] = useState("en");
+  const languageObject = languagesObject;
+  return (
+    <AppContext.Provider
+      value={{
+        state: {
+          languages: languageObject[languageSelected],
+          languageSelected: languageSelected,
+        },
+        setLanguageSelected: setLanguageSelected,
+      }}
+    >
+      <Component {...pageProps} />
+    </AppContext.Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
